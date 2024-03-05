d7d88171e51a43fbb4de726935fea8232b182adc
Ritesh2903 <107747538+Ritesh2903@users.noreply.github.com>
2024-03-05 23:00:33 +0530

    Update MovieCard.jsx

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ import "../styles/MovieCard.css";
 export default function MovieCard() {
   return (
     <div className="movie">
-      <img src={"https://via.placeholder.com/175"} alt="Movie Poster" />
+      <img src={"https://thetelugufilmnagar.com/wp-content/uploads/2022/11/Adipurush.webp"} alt="Movie Poster" />
       <div className="details">
         <h2>Adipurush</h2>
         <p>Description Description Description Description Description</p>
