8794c14d1af900cc97152d2df2a0c738eafdd525
Ankur <ankur.musmade.kctvn@gmail.com>
2024-04-12 12:52:19 +0530

    done with reserving seats after payment

--- frontend/src/pages/ticket.js
+++ frontend/src/pages/ticket.js
@@ -3,55 +3,58 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const TicketDetails = () => {
-  const { txnId , showId, seats}= useParams();
+  const currentUrl = window.location.href;
+
+const pathParts = currentUrl.split('/');
+
+const seatInfo = pathParts[pathParts.length - 1];
+
+const seats = seatInfo.split(',');
+
+  const { txnId , showId, seatss}= useParams();
 
   const [paymentData, setPaymentData] = useState(null);
+
 
   const fetchPaymentStatus = async () => {
     try {
       const response = await axios.get(`http://localhost:4002/api/payment/status/${txnId}/${showId}/${seats}`);
-      setPaymentData(response.data);
-
-      if(paymentData.status === "SUCCESS"){
-        reserveSeats(seats);
+      const paymentData = response.data;
+      setPaymentData(paymentData); // Update the state with payment data
+  
+      // Check the status after setting paymentData
+      if (paymentData.status === "SUCCESS") {
+        reserveSeats(seats); // Call reserveSeats if status is SUCCESS
       }
-
-
     } catch (error) {
-      console.log('Error fetching payment status:', error);
+      console.log('Error fetching payment status:', error.message);
     }
   };
+  
 
   const reserveSeats = async (seats) => {
-    // console.log(seats);
     try {
       for (var i = 0; i < seats.length; i++) {
-        // console.log(_id);
         const response = await axios.patch(
           `/api/seat/patchseatsbyshowId/${showId}`,
           { showId, seatNumber: seats[i] }
         );
-        console.log("reserve seats response :- ",response.data);
+        // console.log("reserve seats response :- ",response.data);
       }
     } catch (error) {
       console.error(error.message);
     }
   };
 
-
-  // useEffect(() => {
-
-  //   fetchPaymentStatus();
-  // }, [paymentStatus]);
+  useEffect(() => {
+    fetchPaymentStatus(); 
+  }, []);
 
   return (
     <div>
       <h2>Tickets Booked Successfully</h2>
       <p>Thank you for booking your tickets. Enjoy the show!</p>
-      <button onClick={fetchPaymentStatus}>Check Payment Status</button>
-      {
-        (paymentData) && <p>{JSON.stringify(paymentData)}</p> 
-      }
+      
     </div>
   );
 };
