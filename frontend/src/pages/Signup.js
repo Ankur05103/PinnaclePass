65e36f10fa298c213cae2928711642d87fc6c170
Nakul Pattewar <pattewarnakul@gmail.com>
2024-04-13 14:21:24 +0530

    Done with appropriate Login/signup Functionality

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,41 +1,41 @@
-import { useState } from "react"
-import "../styles/Login.css"
-import { useSignup } from "../hooks/useSignup"
+// import { useState } from "react"
+// import "../styles/Login.css"
+// import { useSignup } from "../hooks/useSignup"
 
-const Signup = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [mobile, setMobile] = useState('')
-  const {signup,error,isLoading} = useSignup()
+// const Signup = () => {
+//   const [email, setEmail] = useState('')
+//   const [password, setPassword] = useState('')
+//   const [mobile, setMobile] = useState('')
+//   const {signup,error,isLoading} = useSignup()
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
+//   const handleSubmit = async (e) => {
+//     e.preventDefault()
 
-    // console.log(email, password)
-    await signup(email,password,mobile)
-  }
+//     // console.log(email, password)
+//     await signup(email,password,mobile)
+//   }
 
-  return (
-    <form className="login" onSubmit={handleSubmit}>
-      <h2>Sign Up</h2>
+//   return (
+//     <form className="login" onSubmit={handleSubmit}>
+//       <h2>Sign Up</h2>
       
-      <label>Email address:</label>
-      <input 
-        type="email" 
-        onChange={(e) => setEmail(e.target.value)} 
-        value={email} 
-      />
-      <label>Password:</label>
-      <input 
-        type="password" 
-        onChange={(e) => setPassword(e.target.value)} 
-        value={password} 
-      />
+//       <label>Email address:</label>
+//       <input 
+//         type="email" 
+//         onChange={(e) => setEmail(e.target.value)} 
+//         value={email} 
+//       />
+//       <label>Password:</label>
+//       <input 
+//         type="password" 
+//         onChange={(e) => setPassword(e.target.value)} 
+//         value={password} 
+//       />
 
-      <button disabled={isLoading}>Sign up</button>
-      {error && <div className="error">{error}</div>}
-    </form>
-  )
-}
+//       <button disabled={isLoading}>Sign up</button>
+//       {error && <div className="error">{error}</div>}
+//     </form>
+//   )
+// }
 
-export default Signup
+// export default Signup
